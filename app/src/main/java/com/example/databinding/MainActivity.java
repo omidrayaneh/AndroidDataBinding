c8f18d12f880913baefb80ad91710a623bc2d24e
omidrayaneh <omidrayaneh@gmail.com>
2020-02-20 00:33:18 +0330

    button click even

--- app/src/main/java/com/example/databinding/MainActivity.java
+++ app/src/main/java/com/example/databinding/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.databinding;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.databinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding activityMainBinding;
+    private ActivityMainBinding activityMainBinding;
+    private MainActivityClickHandler handlers;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
 
         activityMainBinding= DataBindingUtil.setContentView(this,R.layout.activity_main);
         activityMainBinding.setUser(getCurrentUser());
+
+        handlers=new MainActivityClickHandler(this);
+        activityMainBinding.setClickHandler(handlers);
     }
 
     private User getCurrentUser(){
@@ -24,5 +31,21 @@ public class MainActivity extends AppCompatActivity {
         user.setUserName("omid");
         user.setUserEmail("omidrayaneh@gmail.com");
         return user;
+    }
+
+    public class MainActivityClickHandler {
+
+        Context context;
+
+        public MainActivityClickHandler(Context context) {
+            this.context = context;
+        }
+
+        public void AllowButtonCliked(View view){
+            Toast.makeText(context,"Allow button clicked!",Toast.LENGTH_LONG ).show();
+        }
+        public void CancelButtonCliked(View view){
+            Toast.makeText(context,"Cancel button clicked!",Toast.LENGTH_LONG ).show();
+        }
     }
 }
